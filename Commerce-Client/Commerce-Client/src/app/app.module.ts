4cfd257f5837cd616899da55309945f6ae62c4df
matth1073 <matth1073@gmail.com>
2020-02-29 17:05:53 -0600

    issue #9

--- Commerce-Client/Commerce-Client/src/app/app.module.ts
+++ Commerce-Client/Commerce-Client/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
