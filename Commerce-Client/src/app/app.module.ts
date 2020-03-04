d14855a54d37a94350f67682eee91dd0ff26a1e9
koverr <kso3xd@mail.umkc.edu>
2020-03-03 22:11:44 -0600

    Recreated angular project with bootstrap in mind. Cleaned up some folders and file structure

---
+++ Commerce-Client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TransactionsComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
