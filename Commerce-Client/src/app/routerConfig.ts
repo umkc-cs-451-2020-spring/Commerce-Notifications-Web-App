bd0084ed694f2cec92c09b17d2e88797ff2829d5
koverr <kso3xd@mail.umkc.edu>
2020-03-13 16:12:16 -0500

    Added routerconfig for notifications page

--- Commerce-Client/src/app/routerConfig.ts
+++ Commerce-Client/src/app/routerConfig.ts
@@ -2,18 +2,23 @@
 
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 import { TransactionsComponent } from './transactions/transactions.component';
 
 const appRoutes: Routes = [
-    {   
+    {
         path: '',
         redirectTo: '/login',
         pathMatch: 'full'
     },
-    {   
-        path: 'login', 
+    {
+        path: 'login',
         component: LoginComponent,
         pathMatch: 'full'
+    },
+    {
+        path: 'notifications',
+        component: NotificationsComponent
     },
     {
         path: 'transactions',
