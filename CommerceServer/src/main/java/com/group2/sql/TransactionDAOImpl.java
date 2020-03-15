56e96f4fbeb73f1f6cc4b7f0ff81b6e115bdeb5b
monarch8 <connorlm1997@gmail.com>
2020-03-15 13:31:59 -0500

    Fixed database connection string and dependencies to fix build error

--- CommerceServer/src/main/java/com/group2/sql/TransactionDAOImpl.java
+++ CommerceServer/src/main/java/com/group2/sql/TransactionDAOImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
