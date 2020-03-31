e40d67b7c7a39319cb5b940ff21a9a0cc72011a9
matth1073 <matth1073@gmail.com>
2020-03-30 19:09:12 -0500

    added rule component

---
+++ Commerce-Client/src/app/rule/rule.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit} from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-rule',
+  templateUrl: './rule.component.html',
+  styleUrls: ['./rule.component.css']
+})
+export class RuleComponent implements OnInit {
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  ngOnInit(): void {
+  }
+
+  openAmount() {
+    activeModal.dismiss();
+  }
+
+}
