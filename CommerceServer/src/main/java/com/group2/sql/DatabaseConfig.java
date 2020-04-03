45a4320e544ee971cd865d0db8ff33b98216ea7c
monarch8 <connorlm1997@gmail.com>
2020-04-03 15:11:27 -0500

    Changed database config

--- CommerceServer/src/main/java/com/group2/sql/DatabaseConfig.java
+++ CommerceServer/src/main/java/com/group2/sql/DatabaseConfig.java
@@ -10,7 +10,7 @@ public class DatabaseConfig {
 
 		DriverManagerDataSource database = new DriverManagerDataSource();
 		database.setDriverClassName("com.mysql.jdbc.Driver");
-		database.setUrl("jdbc:mysql://136.32.145.118:3306/CS_451?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+		database.setUrl("jdbc:mysql://136.32.145.118:3306/CommerceDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
 		database.setUsername("connor");
 		database.setPassword("c0nn0r1");
 
