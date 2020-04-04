4938fa8ff7ad4f6e4e647157b8ef3fbc4273c27b
koverr <kso3xd@mail.umkc.edu>
2020-04-04 17:24:54 -0500

    Fixed springboot proxy config

--- Commerce-Client/src/app/services/transaction.service.ts
+++ Commerce-Client/src/app/services/transaction.service.ts
@@ -9,7 +9,7 @@ const httpOptions = {
     providedIn: 'root'
 })
 export class TransactionService {
-    private testSpringUrl = 'http://localhost:8080/Commerce-Server/api/transactions/test';
+    private testSpringUrl = 'api/transactions/test';
 
     constructor(private http: HttpClient) { }
 
