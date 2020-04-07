fb5060f30316fa3df203cfa55e4b21212b2eda28
koverr <kso3xd@mail.umkc.edu>
2020-04-06 20:06:40 -0500

    Removed some commented code

--- Commerce-Client/src/app/transactions/transactions.component.ts
+++ Commerce-Client/src/app/transactions/transactions.component.ts
@@ -3,12 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Transaction } from '../models/transaction';
 import { TransactionService } from '../services/transaction.service';
 
-// const TRANSACTIONS: Transaction[] = [
-//   {account: 'Checking', date: '02/29/20', balance: 245.05, amount: 12.24, description: 'Panda Express'},
-//   {account: 'Checking', date: '03/01/20', balance: 232.81, amount: 6.45, description: 'Walmart'},
-//   {account: 'Checking', date: '03/01/20', balance: 226.36, amount: 7.87, description: 'Hy-Vee'},
-// ];
-
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
@@ -17,7 +11,6 @@ import { TransactionService } from '../services/transaction.service';
 export class TransactionsComponent implements OnInit {
   transactions: Transaction[];
   constructor(private transactionService: TransactionService) {
-    // this.testSpring();
     this.getTransactions();
    }
 
@@ -29,10 +22,4 @@ export class TransactionsComponent implements OnInit {
       this.transactions = transaction;
     });
   }
-
-  // testSpring() {
-  //   this.transactionService.testSpring().subscribe(transaction => {
-  //     console.log(transaction);
-  //   });
-  // }
 }
