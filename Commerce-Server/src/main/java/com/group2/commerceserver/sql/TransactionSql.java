a90a741d9aa00558c84f47b4c7a242bdbbb7e18b
koverr <kso3xd@mail.umkc.edu>
2020-04-11 11:43:14 -0500

    Ordered transactions descending by date

--- Commerce-Server/src/main/java/com/group2/commerceserver/sql/TransactionSql.java
+++ Commerce-Server/src/main/java/com/group2/commerceserver/sql/TransactionSql.java
@@ -3,7 +3,8 @@ package com.group2.commerceserver.sql;
 public class TransactionSql {
 
 	public static final String GET_USER_TRANSACTIONS = 
-			"SELECT t.AccountNumber, t.ProcessingDate, a.Balance, t.Amount, t.Description\r\n" + 
-			"FROM Transaction t JOIN Account a ON t.AccountNumber = a.AccountNumber\r\n" + 
-			"WHERE a.UserID = ?;";
+			"SELECT t.AccountNumber, t.ProcessingDate, a.Balance, t.Amount, t.Description " + 
+			"FROM Transaction t JOIN Account a ON t.AccountNumber = a.AccountNumber " + 
+			"WHERE a.UserID = ? " +
+			"ORDER BY t.ProcessingDate DESC;";
 }
