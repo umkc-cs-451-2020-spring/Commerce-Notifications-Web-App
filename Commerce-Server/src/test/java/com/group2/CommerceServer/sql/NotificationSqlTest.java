2c6366f1d82ceecd063f69da29e048899f77160b
koverr <kso3xd@mail.umkc.edu>
2020-05-03 20:32:16 -0500

    Fixed filter string Junit

--- Commerce-Server/src/test/java/com/group2/CommerceServer/sql/NotificationSqlTest.java
+++ Commerce-Server/src/test/java/com/group2/CommerceServer/sql/NotificationSqlTest.java
@@ -23,7 +23,7 @@ public class NotificationSqlTest {
 		rule.setCategory("Food");
 		
 		String expectedResult = NotificationSql.buildTriggerString(rule);
-		assertEquals(expectedResult, "CREATE TRIGGER CommerceDB.TestTrigger AFTER INSERT ON CommerceDB.Transaction FOR EACH ROW BEGIN IF NEW.Amount > :amount AND NEW.Balance < :balance AND NEW.State != :location AND TIME(NEW.ProcessingDate) BETWEEN :startTime AND :endTime AND NEW.Category = :category AND NEW.AccountNumber IN (SELECT AccountNumber FROM CommerceDB.Account WHERE UserID = :userId ) THEN INSERT INTO CommerceDB.Notifications(TriggerID, TransactionID, Message, ReadStatus) VALUES((SELECT TriggerID FROM CommerceDB.Trigger WHERE TriggerName = :triggerName), NEW.TransactionID, CONCAT('Transaction at ', NEW.Description, ' is over $', :amount , ' and', ' made balance in account #', NEW.AccountNumber, ' under $', :balance , ' and', ' occurred outside of ', :location , ' and', ' occurred between ', :startTime , ' and ', :endTime , ' and' ' is a ', :category , ' transaction', '.'), false); UPDATE CommerceDB.Trigger SET TriggerCount = TriggerCount + 1 WHERE TriggerName = :triggerName; END IF; END");
+		assertEquals("CREATE TRIGGER CommerceDB.TestTrigger AFTER INSERT ON CommerceDB.Transaction FOR EACH ROW BEGIN IF NEW.Amount > :amount AND NEW.Balance < :balance AND NEW.State != :location AND TIME(NEW.ProcessingDate) BETWEEN :startTime AND :endTime AND NEW.Category = :category AND NEW.AccountNumber IN (SELECT AccountNumber FROM CommerceDB.Account WHERE UserID = :userId ) THEN INSERT INTO CommerceDB.Notifications(TriggerID, TransactionID, Message, ReadStatus) VALUES((SELECT TriggerID FROM CommerceDB.Trigger WHERE TriggerName = :triggerName), NEW.TransactionID, CONCAT('Transaction at ', NEW.Description, ' is over $', :amount , ' and', ' made balance in account #', NEW.AccountNumber, ' under $', :balance , ' and', ' occurred outside of ', :location , ' and', ' occurred between ', :startTime , ' and ', :endTime , ' and' ' is a ', :category , ' transaction', '.'), false); END IF; END", expectedResult);
 	}
 	
 	@Test
@@ -33,9 +33,9 @@ public class NotificationSqlTest {
 		filter.setStartDate("2020-04-19 12:00:00");
 		filter.setEndDate("2020-04-19 12:00:00");
 		filter.setHasNotifications(true);
+		filter.setUnread(true);
 		
 		String expectedResult = NotificationSql.buildFiltersString(filter);
-		assertEquals(expectedResult,"SELECT tab.TriggerID, tab.TriggerName, tab.TriggerCount FROM ( SELECT a.TriggerID, a.TriggerName, (SELECT COUNT(t.TransactionID) FROM CommerceDB.Notifications as n JOIN CommerceDB.Transaction as t ON n.TransactionID = t.TransactionID WHERE n.TriggerID = a.TriggerID AND t.ProcessingDate BETWEEN :startDate AND :endDate ) AS TriggerCount FROM CommerceDB.Trigger AS a WHERE a.UserID = :userId ) tab WHERE tab.TriggerCount > 0;");
+		assertEquals("SELECT tab.TriggerID, tab.TriggerName, tab.TriggerCount FROM ( SELECT a.TriggerID, a.TriggerName, (SELECT COUNT(t.TransactionID) FROM CommerceDB.Notifications as n JOIN CommerceDB.Transaction as t ON n.TransactionID = t.TransactionID WHERE n.TriggerID = a.TriggerID AND t.ProcessingDate BETWEEN :startDate AND :endDate AND n.ReadStatus = FALSE) AS TriggerCount FROM CommerceDB.Trigger AS a WHERE a.UserID = :userId ) tab WHERE tab.TriggerCount > 0;", expectedResult);
 	}
-
 }
